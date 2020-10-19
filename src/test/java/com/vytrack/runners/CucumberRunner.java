382abebdba30623699e8763b4aa77a91cfd85850
erkamow <erkamafsin@afsteknik.com>
2020-10-19 21:26:16 +0300

    day1_Cucumber

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,15 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src\\test\\resources\\features",// copied from content root "feature"
+                   glue = "com\\vytrack\\step_definitions",  // copied from source root " step definitions"
+                    dryRun = true)
+
+public class CucumberRunner {
+    // running cucumber test scenarios
+
+}
