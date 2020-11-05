44288f2e69fbbf00da8922dae6aa1511b3e6c0a3
erkamow <erkamafsin@afsteknik.com>
2020-11-05 19:42:30 +0300

    Cucumber BDD scenerioOutline

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -1,18 +1,27 @@
 package com.vytrack.runners;
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\features",// copied from content root "feature"
-                   glue = "com\\vytrack\\step_definitions",  // copied from source root " step definitions"
-                    dryRun = false,
-                    tags = "@calendar_events",
-                    publish = true)
-
-
+@CucumberOptions(
+        plugin = {
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@with_two_columns",
+        publish = true
+)
 public class CucumberRunner {
-    // running cucumber test scenarios
+    //tags = "@s_o and @with_two_columns",
+    // scenario must have BOTH tags
+    //java logic if(@s_o && @with_two_columns)
 
+    //tags = "@s_o or @with_two_columns",
+    // scenario must have EITHER OR tags
+    //java logic if(@s_o || @with_two_columns),
 }
