d4a0b42889dfef997d8aded35aabf63c1212897e
erkamow <erkamafsin@afsteknik.com>
2020-11-09 15:00:33 +0300

    selenium grid

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -17,11 +17,11 @@ import org.junit.runner.RunWith;
         publish = true
 )
 public class CucumberRunner {
-    //tags = "@s_o and @with_two_columns",
-    // scenario must have BOTH tags
-    //java logic if(@s_o && @with_two_columns)
+        //tags = "@s_o and @with_two_columns",
+        // scenario must have BOTH tags
+        //java logic if(@s_o && @with_two_columns)
 
-    //tags = "@s_o or @with_two_columns",
-    // scenario must have EITHER OR tags
-    //java logic if(@s_o || @with_two_columns),
+        //tags = "@s_o or @with_two_columns",
+        // scenario must have EITHER OR tags
+        //java logic if(@s_o || @with_two_columns),
 }
